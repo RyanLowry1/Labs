05181cd38c1b5afd6aefda0994442716f6020317
RyanLowry1 <C00305950@setu.ie>
2025-09-18 10:49:34 +0100

    Add files via upload

---
+++ lab2/HotelRoom.java
@@ -0,0 +1,73 @@
+public class HotelRoom {
+
+    private int roomNumber;
+    private String roomType;
+    private boolean vacant;
+    private double rate;
+
+    public HotelRoom(){ 
+    setRoomNumber(0); 
+    setRoomType(""); 
+    setVacant(false); 
+    setRate(0);
+    }
+
+
+    public HotelRoom(int roomNumber, String roomType) {
+    setRoomNumber(roomNumber);
+    setRoomType(roomType);
+    }
+
+
+    public HotelRoom(int roomNumber, String roomType, boolean vacant, double rate){ 
+    setRoomNumber(roomNumber);
+    setRoomType(roomType);
+    setVacant(false);
+    setRate(rate);
+    }
+
+    public int getRoomNumber(){
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber){
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType(){
+        return roomType;
+    }
+
+    public void setRoomType(String roomType){
+        this.roomType = roomType;
+    }
+
+    public boolean getVacant(){
+        return vacant;
+    }
+
+    public void setVacant(boolean vacant){
+        this.vacant = vacant;
+    }
+
+    public double getRate(){
+        return rate;
+    }
+
+    public void setRate(double rate){
+        this.rate = rate;
+    }
+
+    public String toString(){
+        return "HotelRoom [roomNumber=" + roomNumber + ", roomType=" + roomType + ", vacant="+ vacant +", rate="
+        + rate +"]";
+    }
+    
+    public void setOccupied(boolean occupied) {
+        this.vacant = !occupied;
+    }
+    public boolean isOccupied() {
+        return !vacant;
+}
+
+}
