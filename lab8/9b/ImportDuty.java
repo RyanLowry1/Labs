055f4ec46a61689d7f631251f5dd4591015e28a5
RyanLowry1 <C00305950@setu.ie>
2025-12-04 13:57:57 +0000

    Delete lab8/9b directory

--- lab8/9b/ImportDuty.java
+++
@@ -1,9 +0,0 @@
-// Name: Ryan Lowry
-// Date : 28/11/2025
-// Student Number : C00305950
-public interface ImportDuty{
-    double CARTAXRATE = 0.10;   
-    double HGVTAXRATE = 0.15; 
-
-    double calculateDuty();
-}
